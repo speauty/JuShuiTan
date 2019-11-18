ccf308f89ca0545dda90ec479b7550f71728a8b1
speauty <speauty@163.com>
2019-11-18 16:59:28 +0800

    初次建composer包, 提交简单类库测试

---
+++ src/Client.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author:  Speauty
+ * Email:   speauty@163.com
+ * File:    Client.php
+ * Created: 2019/11/18 下午4:57
+ */
+
+namespace JuShuiTan;
+
+
+class Client
+{
+    public function test()
+    {
+        echo 'hah';
+    }
+}
